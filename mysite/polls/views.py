af8ac7c037922dbde33becea6e009b8d2a7c4c89
Juan Manuel <juanmanuelsemper@gmail.com>
2024-04-12 12:24:49 -0300

    [Parte 4.3] Modifique las vistas.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .models import Choice, Question
 from django.template import loader
 from django.http import Http404
 from django.db.models import F
 from django.urls import reverse
+from django.views import generic
 
 
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
